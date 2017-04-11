25c04f3f561aac18555579560f43b608384ddd15
jmzamorab <josem.zamorab@gmail.com>
2017-04-12 00:50:03 +0200

    capto foto y muestro de la galeria

--- app/src/main/java/org/jordi/pruebafacebook2017sdk1/MainActivity.java
+++ app/src/main/java/org/jordi/pruebafacebook2017sdk1/MainActivity.java
@@ -29,23 +29,25 @@ import android.widget.TextView;
 }
 */
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -56,10 +58,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import static android.R.attr.data;
-import static android.R.attr.width;
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class MainActivity extends AppCompatActivity {
     private String tag = "MainActivity";
@@ -75,7 +73,8 @@ public class MainActivity extends AppCompatActivity {
     // doc Internet
     private static int TAKE_PICTURE = 1;
     private static int SELECT_PICTURE = 2;
-
+    private Button shareFacebookBtn;
+    private Button shareTwitterBtn;
 
 
     static {
@@ -84,14 +83,15 @@ public class MainActivity extends AppCompatActivity {
 
     // funciones de Código nativo
     public native void convertirGrises(Bitmap bitmapIn, Bitmap bitmapOut);
+
     public native void convertirSepia(Bitmap bitmapIn, Bitmap bitmapOut);
+
     public native void creaMarco(Bitmap bitmapIn, Bitmap bitmapOut);
-    // public native boolean callback();
+
     public native void creaMarcoCallBack(Bitmap bitmapIn, Bitmap bitmapOut);
 
     public static boolean hayPixel(int x, int y) {
-        // Log.d(" *** hayPixel()", " Invocado con x => " + x + " y => " + y + " x%10 == y%10 => " + (x%10 == y%10));
-        return x%10 == y%10;
+        return x % 10 == y % 10;
     }
 
     @Override
@@ -100,16 +100,39 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(tag, "Imagen antes de modificar");
         ivDisplay = (ImageView) findViewById(R.id.ivDisplay);
+        shareFacebookBtn = (Button) findViewById(R.id.shareFacebookBtn);
+        shareTwitterBtn = (Button) findViewById(R.id.shareTwitterBtn);
+        shareFacebookBtn.setEnabled(true);
+        shareTwitterBtn.setEnabled(true);
         setColor(true);
+       /* shareFacebookBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FacebookActivity.class);
+                i.putExtra("photo",bitmapOriginal);
+                startActivity(i);
+            }
+        });
+
+        shareTwitterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, FacebookActivity.class);
+                i.putExtra("photo",bitmapOriginal);
+                startActivity(i);
+            }
+        });*/
+
+        //setColor(true);
     }
 
-    @Override public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
     }
 
-    private void setColor(Boolean imgOrigen)
-    {
+    private void setColor(Boolean imgOrigen) {
         Log.i(tag, "Resetear Imagen");
         if (imgOrigen) {
             BitmapFactory.Options options = new BitmapFactory.Options();
@@ -121,16 +144,17 @@ public class MainActivity extends AppCompatActivity {
         ivDisplay.setImageBitmap(bitmapOriginal);
     }
 
-    @Override public boolean onOptionsItemSelected(MenuItem item) {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
-        switch (id){
-            case  R.id.action_reset:
+        switch (id) {
+            case R.id.action_reset:
                 setColor(true);
                 break;
             case R.id.color:
                 setColor(false);
                 break;
-            case  R.id.gray:
+            case R.id.gray:
                 Log.i(tag, "Conversion a escala de grises");
                 bitmapGrises = Bitmap.createBitmap(bitmapOriginal.getWidth(), bitmapOriginal.getHeight(), Bitmap.Config.ARGB_8888);
                 convertirGrises(bitmapOriginal, bitmapGrises);
@@ -166,55 +190,79 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        Log.e("pasa por aquí", "sii");
 
+        if (permissions[0] == Manifest.permission.CAMERA && grantResults[0] == PackageManager.PERMISSION_DENIED) {
+            Toast.makeText(MainActivity.this, "Permiso denegado para acceder a la cámara", Toast.LENGTH_SHORT);
+        }
+        else  takePhoto();
+    }
 
     public void onTakePhoto(View v) {
         //tomarFoto();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            takePhoto();
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+        }
+
+
+    }
+
+    private void takePhoto()
+    {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         int code = TAKE_PICTURE;
-        try {
-            File fic =  createImageFile();
-            Uri output = Uri.fromFile(fic);
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, output);
-            startActivityForResult(takePictureIntent, code);
+        //try {
+          //  File fic = createImageFile();
+           // Uri output = Uri.fromFile(fic);
+            //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, output);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, code);}
 //        } else if (rbtnGallery.isChecked()){
 //            intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI);
 //            code = SELECT_PICTURE;
 //        }
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(),"No se ha podido capturar la fotografía de forma correcta " + e.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
+        //} catch (IOException e) {
+         //   Toast.makeText(getApplicationContext(), "No se ha podido capturar la fotografía de forma correcta " + e.getMessage(), Toast.LENGTH_LONG).show();
+        //}
     }
 
-
     //Crea un fichero que su nombre para hacer único
-    private File createImageFile() throws IOException {
+ /*   private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
         File image = File.createTempFile(
-                imageFileName,  /* prefix */
-                ".jpg",         /* suffix */
-                storageDir      /* directory */
+                imageFileName,  /* prefix
+                ".jpg",         /* suffix
+                storageDir      /* directory
         );
 
         // Save a file: path for use with ACTION_VIEW intents
         mCurrentPhotoPath = image.getAbsolutePath();
         return image;
-    }
+    }*/
 
 
     // Al tomar fotografía, comprobamos si resultado correcto
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == TAKE_PICTURE && resultCode == RESULT_OK) {
-            if (data == null) {
-                escalaImagen();
-            }
-        }
-        else if (requestCode == SELECT_PICTURE){
+          //  if (data == null) {
+                Bundle extras = data.getExtras();
+                //bitmapOriginal = (Bitmap) extras.get("data");
+                //ivDisplay.setImageBitmap(bitmapOriginal);
+                //bitmapFilter = bitmapOriginal;
+                //Drawable drawable = ivDisplay.getDrawable();
+                //BitmapDrawable bitmapDrawable = ((BitmapDrawable) drawable);
+                //photo = bitmapDrawable .getBitmap();
+                escalaImagen((Bitmap) extras.get("data"));
+           // }
+        } else if (requestCode == SELECT_PICTURE) {
             Uri selectedImage = data.getData();
             InputStream is;
             try {
@@ -222,21 +270,25 @@ public class MainActivity extends AppCompatActivity {
                 BufferedInputStream bis = new BufferedInputStream(is);
                 Bitmap bitmap = BitmapFactory.decodeStream(bis);
                 //       ImageView iv = (ImageView)findViewById(R.id.imgView);
-                bitmapOriginal = bitmap;
-                setColor(false);
-            } catch (FileNotFoundException e) {}
+                //bitmapOriginal = bitmap;
+                escalaImagen(bitmap);
+                //setColor(false);
+            } catch (FileNotFoundException e) {
+            }
+            //Bundle extras = data.getExtras();
+            //escalaImagen((Bitmap) extras.get("data"));
         }
     }
 
     // Escalamos la fotografía, y la mostramos
-    private void escalaImagen()
-    {
+    private void escalaImagen(Bitmap bitmap) {
         int targetW = ivDisplay.getWidth();
         int targetH = ivDisplay.getHeight();
         BitmapFactory.Options bmOptions = new BitmapFactory.Options();
         BitmapFactory.decodeFile(mCurrentPhotoPath, bmOptions);
-        Bitmap resizedBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath), targetW, targetH, true);
-        bitmapOriginal = resizedBitmap;
+        bitmapOriginal = Bitmap.createScaledBitmap(bitmap, targetW, targetH, true);
+//        Bitmap resizedBitmap = Bitmap.createScaledBitmap(BitmapFactory.decodeFile(mCurrentPhotoPath), targetW, targetH, true);
+ //       bitmapOriginal = resizedBitmap;
         ivDisplay.setImageBitmap(bitmapOriginal);
     }
 }
